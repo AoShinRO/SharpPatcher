d5e7ea1e82546d5470ae5c0e7ab59b0985df3b4b
AoShinRO <darklord.dl@gmail.com>
2024-04-01 15:51:09 -0300

    InitialRelease

---
+++ SharpPatcher/Anticheat.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace SharpPatcher
+{
+    /// <summary>
+    /// Lógica interna para Window1.xaml
+    /// </summary>
+    public partial class AntiCheater : Window
+    {
+ 
+        public AntiCheater()
+        {
+            InitializeComponent();
+        }
+
+        private void Progress_Initialized(object sender, EventArgs e)
+        {
+            Duration duration = new Duration(TimeSpan.FromSeconds(2));
+
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = 0; // Start from the current value
+            doubleAnimation.To = 100; // End at the maximum value (100%)
+            doubleAnimation.Duration = duration;
+            doubleAnimation.Completed += (s, _) =>
+            {
+                Close();
+            };
+            // Apply the animation to the ProgressBar's Value property
+            progress.BeginAnimation(ProgressBar.ValueProperty, doubleAnimation);
+        }
+    }
+}
---
+++ SharpPatcher/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace SharpPatcher
+{
+
+    /// <summary>
+    /// Interação lógica para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // My code goes here, but nothing ever happens.
+
+            Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(Current_DispatcherUnhandledException);
+            DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(App_DispatcherUnhandledException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+        }
+
+        void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            // we cannot handle this, but not to worry, I have not encountered this exception yet.  
+            // However, you can show/log the exception message and show a message that if the application is terminating or not.  
+            var isTerminating = e.IsTerminating;
+        }
+    }
+
+
+}
